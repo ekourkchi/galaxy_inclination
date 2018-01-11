5cefc946573bccb2e262e805b6666fd756ad08a5
ekourkchi <ekourkchi@gmail.com>
2018-01-10 21:49:44 -1000

    middle pannels only narrow down the search, edge pannels open up wider ranges (5 degrees), if std(inc)<0.5 then target galaxy is added, and no further exhausting search

--- inclination_std.py
+++ inclination_std.py
@@ -751,7 +751,7 @@ if __name__ == '__main__':
        unexpected = False
        
        while status==2: # redo
-           if True: # try: 
+           try: 
              list, status = insert_value(list, value, 0, N_max, N_max, 4)
              if value.flag > 0 :   # flagged
                  print
@@ -763,10 +763,10 @@ if __name__ == '__main__':
                        value.reason = choice 
                        print "pgc"+str(value.pgc)+' was flagged successfully ...'
                        list = insert_lst(list, value, 0)
-           #except:
-             #print "GUI was closed unexpectedly ..."
-             #unexpected = True
-             #status=0
+           except:
+             print "GUI was closed unexpectedly ..."
+             unexpected = True
+             status=0
            
 
        if status<2 and not unexpected:   #  1:flag, 0:done
